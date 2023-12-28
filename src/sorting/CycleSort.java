bc31c5425837968ee3d46300ed61efa83ddd647f
Anurag Sisodiya <122148320+ANURAGSISODIYAA@users.noreply.github.com>
2023-12-29 04:52:44 +0530

    cycle Sort

--- src/sorting/CycleSort.java
+++ src/sorting/CycleSort.java
@@ -1,5 +1,30 @@
-package src;
+package src.sorting;
+
+import java.util.Arrays;
 
 public class CycleSort {
 
+    public static void main(String[] abc){
+        int[] arr = {5, 3, 4, 1, 2};
+        cycle(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void cycle(int arr[]){
+       int i=0;
+        while(i<arr.length) {
+            int correct =arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, arr[i] - 1);
+            }else{
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
